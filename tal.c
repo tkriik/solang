facba7189dca40d31f23138e4812c5bf94f63635
Tanel Kriik <tanel.kriik+github@gmail.com>
2018-08-26 23:31:40 +0300

    Add option parsing

--- tal.c
+++ tal.c
@@ -1,4 +1,6 @@
+#include <getopt.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "repl.h"
 
@@ -6,18 +8,46 @@ static void
 usage(void)
 {
 	extern char *__progname;
-
-	fprintf(stderr, "usage: %s\n", __progname);
+	fprintf(stderr,
+"\n"
+"Open shell: %s [OPTIONS]\n"
+"\n"
+"Run script: %s [OPTIONS] <FILENAME>\n"
+"\n"
+"Options:\n"
+"  -h   Print this help message\n"
+"  -i   Open shell (with or without script)\n"
+"\n",
+	    __progname,
+	    __progname);
+	exit(1);
 }
 
 int
 main(int argc, char **argv)
 {
-	if (argc != 1) {
-		usage();
+	int interactive = 0;
+
+	int opt;
+	while ((opt = getopt(argc, argv, "hi")) != -1) {
+		switch (opt) {
+		case 'i':
+			interactive = 1;
+			break;
+		case 'h':
+		default:
+			usage();
+			break;
+		}
 	}
 
-	repl_enter();
+	argc -= optind;
+	argv += optind;
+
+	if (argc == 0)
+		repl_enter();
+
+	printf("eval %s\n", argv[0]);
 
 	return 0;
 }
