718f47483473f2db7a0789b16fdd721dcdecd368
Tanel Kriik <tanel.kriik+github@gmail.com>
2018-09-26 12:50:31 +0300

    Use rpds list in read

--- src/eval.rs
+++ src/eval.rs
@@ -34,59 +34,64 @@ pub fn eval(env: &mut Env, sx: &Sx) -> Result<Sx, EvalError> {
             }
         },
 
-        Sx::List(l) => {
-            match l.first() {
-                Some(Sx::Symbol(name)) if name.as_ref() == "def" => {
-                    return do_def(env, sx);
-                },
-
-                _ => {
-                    return Err(EvalError::Undefined(sx.clone()));
-                }
-            }
+        _ => {
+            assert!(false);
+            return Err(EvalError::Undefined(sx.clone()));
         }
+
+        //Sx::List(l) => {
+        //    match l.first() {
+        //        Some(Sx::Symbol(name)) if name.as_ref() == "def" => {
+        //            return do_def(env, sx);
+        //        },
+
+        //        _ => {
+        //            return Err(EvalError::Undefined(sx.clone()));
+        //        }
+        //    }
+        //}
     }
 }
 
 
 // TODO: refactor
-fn do_def(env: &mut Env, sx: &Sx) -> Result<Sx, EvalError> {
-    match sx {
-        Sx::List(l) => {
-            let symbol = &l[1];
-            let exp_arity = 2;
-            let act_arity = l.len() - 1;
-            if exp_arity != act_arity {
-                return Err(EvalError::Arity(symbol.clone(), exp_arity, act_arity));
-            }
-
-            match env.lookup(symbol) {
-                Some(_) => {
-                    return Err(EvalError::Redefine(symbol.clone()));
-                },
-
-                None => {
-                    let value = &l[2];
-                    match eval(env, value) {
-                        Ok(result) => {
-                            env.define(symbol.clone(), result.clone());
-                            return Ok(symbol.clone());
-                        },
-
-                        error @ Err(_) => {
-                            return error;
-                        }
-                    }
-                }
-            }
-        },
-
-        _ => {
-            assert!(false);
-            return Err(EvalError::Undefined(sx.clone()));
-        }
-    }
-}
+//fn do_def(env: &mut Env, sx: &Sx) -> Result<Sx, EvalError> {
+//    match sx {
+//        Sx::List(l) => {
+//            let symbol = &l[1];
+//            let exp_arity = 2;
+//            let act_arity = l.len() - 1;
+//            if exp_arity != act_arity {
+//                return Err(EvalError::Arity(symbol.clone(), exp_arity, act_arity));
+//            }
+//
+//            match env.lookup(symbol) {
+//                Some(_) => {
+//                    return Err(EvalError::Redefine(symbol.clone()));
+//                },
+//
+//                None => {
+//                    let value = &l[2];
+//                    match eval(env, value) {
+//                        Ok(result) => {
+//                            env.define(symbol.clone(), result.clone());
+//                            return Ok(symbol.clone());
+//                        },
+//
+//                        error @ Err(_) => {
+//                            return error;
+//                        }
+//                    }
+//                }
+//            }
+//        },
+//
+//        _ => {
+//            assert!(false);
+//            return Err(EvalError::Undefined(sx.clone()));
+//        }
+//    }
+//}
 
 #[cfg(test)]
 mod tests {
